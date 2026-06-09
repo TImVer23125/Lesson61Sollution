cb90ebfc224a49e93e0a2b20aadd53e3cbdbe1b5
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-09 19:32:38 +0300

    test06

--- Task01/test06.cpp
+++ Task01/test06.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
 
-#define SIZE 5
+void init_random(int* array, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		*(array + i) = rand() % 100;
+	}
+
+}
 
 int main() {
-	int size;
+	int size = 10;
 
-	cout << "Input size of array: ";
-	cin >> size;
+	//cout << "Input size of array: ";
+	//cin >> size;
 
-	int* array = new int[size] {};
+	//int* array = new int[size] {};
+
+	int array[10];
+
+	init_random(array, size);
 
 	for (int i = 0; i < size; i++)
 	{
