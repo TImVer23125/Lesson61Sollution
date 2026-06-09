b2c4a0504c6e565204e6222dba7fa46c65f99880
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-06-09 17:52:26 +0300

    Add project files.

---
+++ Task01/test01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int* ptr = NULL;
+	int number = 10;
+
+	ptr = &number;
+
+	*ptr *= 2;
+
+	cout << &number << " - " << number << endl;
+	cout << ptr << " - " << *ptr << endl;
+	cout << main << " - " << *main << endl;
+	return 0;
+}
